8245d2eb863cd3590ecdab58d260e01d43fbfbfc
Nastena <freziay@mail.ru>
2023-02-01 14:55:03 +0300

    gradleProdgectnew

---
+++ db/src/main/java/DbSetting.java
@@ -0,0 +1,13 @@
+import java.util.UUID;
+
+public class DbSetting {
+
+    private String name;
+    private String password;
+
+    public DbSetting(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+}
